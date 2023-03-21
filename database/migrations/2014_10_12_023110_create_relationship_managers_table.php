c3f89f777c61bb703dd5ffc69f21e6bd7602daa5
official-david <adigwu12>
2023-03-22 00:44:23 +0100

    relationship manager

---
+++ database/migrations/2014_10_12_023110_create_relationship_managers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('relationship_managers', function (Blueprint $table) {
+            $table->id();
+            $table->string('manager_name', 50);
+            $table->string('password', 100);
+            $table->string('email', 50);
+            $table->text('biography')->nullable();
+            $table->string('photo_url', 50)->nullable();
+            $table->string('phone_number', 20)->nullable();
+            $table->string('location', 100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('relationship_managers');
+    }
+};
