8a72b9d1df9682525c8114e9b26d2647711b9638
official-david <adigwu12>
2023-03-22 00:32:06 +0100

    populated user table with more columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,27 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->string('username', 50);
+            $table->string('phone_number', 50)->nullable();
+            $table->string('email', 50)->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('address', 100)->nullable();
+            $table->string('city', 50)->nullable();
+            $table->string('state', 50)->nullable();
+            $table->string('country', 50);
+            $table->string('postal_code', 20);
+            $table->string('account_type', 20);
+            $table->decimal('account_balance', 12, 2);
+            $table->string('investment_style', 50);
+            $table->string('investment_goal', 50);
+            $table->string('investment_horizon', 50);
+            $table->string('employment_status', 50);
+            $table->decimal('annual_income', 12, 2);
+            $table->string('risk_tolerance', 20);
+            $table->decimal('net_worth', 12, 2);
+            $table->decimal('investment_amount', 12, 2);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
