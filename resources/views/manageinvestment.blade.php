d41694112956e531d0748994bc3e10c0d9cafb36
official-david <adigwu12>
2023-03-22 01:16:28 +0100

    added management investment page

---
+++ resources/views/manageinvestment.blade.php
@@ -0,0 +1,87 @@
+<html lang="en">
+          <head>
+            <meta charset="utf-8">
+          
+            <title>Manage Investment</title>
+            <meta name="description" content="Figma htmlGenerator">
+            <meta name="author" content="htmlGenerator">
+            <link href="https://fonts.googleapis.com/css?family=Mulish&display=swap" rel="stylesheet">
+
+            <link rel="stylesheet" href="css/styles.css">
+            
+            <style>
+              /*
+                Figma Background for illustrative/preview purposes only.
+                You can remove this style tag with no consequence
+              */
+              body {background: #E5E5E5; }
+            </style>
+          
+          </head>
+          
+          <body>
+            <div class=e1_28>
+  <div class="e1_29"></div>
+  <div class=e121_46>
+    <div class=e121_47>
+      <div class="e121_48"></div><span  class="e121_49">Lekki Garden Estate Returns</span><span  class="e121_50">Up to $200,000 per Annum</span>
+      <div class=e121_51><span  class="e121_52">#Bonds</span></div>
+    </div>
+    <div class=e121_53>
+      <div class="e121_54"></div><span  class="e121_55">GTBank Shares</span><span  class="e121_56">Up to $350,000 per Annum</span>
+      <div class=e121_57><span  class="e121_58">#Bonds</span></div>
+    </div>
+    <div class=e121_59>
+      <div class="e121_60"></div>
+      <div class=e121_61><span  class="e121_62">#Mutual-Funds</span></div><span  class="e121_63">Real Estate Returns</span><span  class="e121_64">Up to $200,000 per Annum</span>
+    </div>
+  </div>
+  <div class=e121_65>
+    <div class=e121_66>
+      <div class="e121_67"></div>
+      <div class=e121_68><span  class="e121_69">#Stocks</span></div><span  class="e121_70">Apple Stock</span><span  class="e121_71">Up to $580,000 per Annum</span>
+    </div>
+    <div class=e121_72>
+      <div class="e121_73"></div>
+      <div class=e121_74><span  class="e121_75">#Bonds</span></div><span  class="e121_76">Eurobonds</span><span  class="e121_77">Up to $125,000 per Annum</span>
+    </div>
+    <div class=e121_78>
+      <div class="e121_79"></div>
+      <div class=e121_80><span  class="e121_81">#Stocks</span></div><span  class="e121_82">Tesla Stock</span><span  class="e121_83">Up to $432,050 per Annum</span>
+    </div>
+  </div>
+  <div class=e121_95>
+    <div class=e121_96>
+      <div class="e121_97"></div>
+      <div class=e121_98><span  class="e121_99">#Stocks</span></div><span  class="e121_100">Apple Stock</span><span  class="e121_101">Up to $580,000 per Annum</span>
+    </div>
+    <div class=e121_102>
+      <div class="e121_103"></div>
+      <div class=e121_104><span  class="e121_105">#Bonds</span></div><span  class="e121_106">Eurobonds</span><span  class="e121_107">Up to $125,000 per Annum</span>
+    </div>
+    <div class=e121_108>
+      <div class="e121_109"></div>
+      <div class=e121_110><span  class="e121_111">#Stocks</span></div><span  class="e121_112">Tesla Stock</span><span  class="e121_113">Up to $432,050 per Annum</span>
+    </div>
+  </div><span  class="e1_157">New Ins (24)</span>
+  <div class=e5_254>
+    <div class="e1_30"></div><span  class="e1_33">Home</span>
+    <div class=e2_162><span  class="e1_34">Manage Ideas</span></div><span  class="e1_128">Profile</span><span  class="e1_130">Messages</span><span  class="e1_129">Log Out</span><span  class="e121_121">Settings</span>
+    <div class=e1_35><span  class="e1_36">Logo</span></div>
+  </div>
+  <div class=e121_93>
+    <div class=e121_88>
+      <div class=e121_89>
+        <div class="e121_90"></div>
+      </div><span  class="e121_91">Filter</span>
+    </div>
+    <div class=e121_92>
+      <div class="e1_123"></div><span  class="e1_124">Search</span>
+    </div>
+  </div>
+  <div class=e121_94><span  class="e1_32">Manage your Investment Ideas</span>
+    <div class="e1_153"></div>
+  </div>
+</div>
+          </body>
+          </html>
