f23948a8e97686f851cddf49aa82233132965d87
official-david <adigwu12>
2023-03-22 01:21:29 +0100

    added relationship manager login page

---
+++ resources/views/relationshipmanagerlogin.blade.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Mulish&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Relationship Manager Login</title>
+</head>
+<body>
+    <div class="v91_127">
+        <div class="v91_128">
+
+        </div>
+        <div class="v91_129">
+            <div class="v91_130">
+
+            </div>
+            <div class="v91_131">
+                <div class="v91_132">
+                    <div class="v91_133">
+
+                    </div>
+                    <div class="v91_134">
+
+                    </div>
+                    <div class="v91_135">
+
+                    </div>
+                    </div>
+                    <div class="v91_136">
+                        <span class="v91_137">Trusted Digital Investment Ideas Platform</span>
+                        </div>
+                        </div>
+                        </div>
+                        <div class="v91_139">
+                            <div class="v91_140">
+
+                            </div>
+                            <div class="v210_7">
+
+                            </div>
+                            <!-- <div class="v91_141"> -->
+                                
+                                <!-- </div> -->
+                                <div class=e121_141><span  class="e121_142">Relationship Manager Login</span>
+                                <span class="v91_145">Sign in to your account and manage your investments</span>
+                                </div>
+                                <div class="v91_146">
+                                    <div class="v91_147">
+<!--<a href="registrationPage.html">
+                <button class="v1_18" id="get-started"><span class="v1_19">GET STARTED</span></button>
+                </a>-->              
+                                    </div>
+                                    
+                                    <span class="v91_148">Don’t Have an account? <a href="Client Registration.html">Create an Account</a></span>
+                                    
+                                
+                                </div>
+                                
+                                    <div class="v91_149">
+                                        <div class="v91_150">
+
+                                        </div><span class="v91_151">Logo</span>
+                                        </div>
+                                        <form action="">
+                                        <div class="v91_158"> <label for="email"><span class="v91_159">Email Address</span> </label>
+                                        
+                                        <div>
+                                           <input type="text" class="v91_160" name="email" id="email" placeholder="example@gmail.com" required>
+                                        
+                                        </div>
+                                        </div>
+                                        <div class="v91_161"> <label for=""><span class="v91_162">Password</span> </label>
+                                            <div>
+                                                <input type="password" class="v91_163" name="password" id="password" required>
+                                
+                                            </div>
+                                            
+                                    </div>
+                                    <div>
+                                    <input type="checkbox" id="show-password">
+                                    <div>
+                                    <label for="show-password" class="v210_8">Show password</label>
+                                </div>
+                                </div>
+                                <a href="">
+                                <button class="v91_141" id="log-in"><span class="v91_142">NEXT</span></button>
+                                </a>
+                                </form>
+                                <!-- 
+                                    <input type="checkbox" id="show-password">
+                                    <label for="show-password">Show password</label>
+                                -->
+                                   <!-- <span class="v210_10">Show Password</span> -->
+
+
+                                        <!-- <div class="v85_37"><label for="firstname"><span class="v85_33">First Name</span>
+                                <div >
+                                    <input type="text" class="v85_34" id="firstname" name="firstnam" placeholder="Damilola" required>
+                                    </label>
+
+                                </div> --> 
+                                <!-- 
+                                    <input type="password" id="password" name="password" required>
+                                          <input type="checkbox" id="show-password">
+                                    <label for="show-password">Show password</label>
+                                -->
+                                <!--
+                                        <div class="v91_161"> 
+                                            <input type="password" name="password" id="password" required>
+                                            <label for="password"><span class="v91_162">Password</span>
+                                            <input type="checkbox" id="show-password">
+                                            <label for="">Show password</label>
+                                            
+                                        <div>  
+                                            <input type="password" class="v91_163" name="password" id="password" required>
+                                        </label>
+                                        </div>
+                                    -->
+                                        <!--
+                                        </div><input type="checkbox" name="show-password" id="show-password">
+                                        <label for="show-password"><span class="v210_8">Show Password</span></label>
+                                    --> 
+                                    <div>
+                                        <a href="" class="forgetPassword"><span class="v210_11">Forgot Password?</span></a>
+                                    </div>
+                                    
+
+                                        </div>
+                                        </div>
+                                        
+                                        
+</body>
+</html>
+
+
+
+   
+  
