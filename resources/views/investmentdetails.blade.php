c6f8187bcfc21d4ebe183ae417f1b95914c088ae
official-david <adigwu12>
2023-03-22 01:13:52 +0100

    added investment details page

---
+++ resources/views/investmentdetails.blade.php
@@ -0,0 +1,33 @@
+<html lang="en">
+          <head>
+            <meta charset="utf-8">
+          
+            <title>Investment Details</title>
+            <meta name="description" content="Figma htmlGenerator">
+            <meta name="author" content="htmlGenerator">
+            <link href="https://fonts.googleapis.com/css?family=Mulish&display=swap" rel="stylesheet">
+
+            <link rel="stylesheet" href="css/styles.css">
+            
+            <style>
+              /*
+                Figma Background for illustrative/preview purposes only.
+                You can remove this style tag with no consequence
+              */
+              body {background: #E5E5E5; }
+            </style>
+          
+          </head>
+          
+          <body>
+            <div class=e90_50>
+  <div class="e90_51"></div>
+  <div class=e90_52><span  class="ei90_52_205_80">The global market for Blockchain Technology estimated at US$3.4 Billion in the year 2022, is projected to reach a revised size of US$19.9 Billion by 2026, growing at a CAGR of 43% over the analysis period. Financial institutions have been spearheading innovations in the Blockchain technology space and technology companies with a foothold in these companies will do well.</span></div><span  class="e90_53">Investment details</span><span  class="e90_54">Real Estate Returns</span><span  class="e90_55">House</span><span  class="e90_56">1</span><span  class="e90_57">John</span><span  class="e90_58">Type</span><span  class="e90_59">Title</span><span  class="e90_60">Risk Rating</span><span  class="e90_61">Author</span><span  class="e90_62">Publish Date</span><span  class="e90_63">Expiry Date</span><span  class="e90_64">Currency</span><span  class="e90_65">Region</span><span  class="e90_66">Country</span><span  class="e90_67">Company</span><span  class="e90_68">31/1/2023</span><span  class="e90_69">5/2/2023</span><span  class="e90_70">USD, EUR, INR</span><span  class="e90_71">Americas, Europe, Asia</span><span  class="e90_72">United States of America, Germany, India</span><span  class="e90_73">Blockchain</span><span  class="e90_74">Description</span>
+  <div class=e90_75>
+    <div class="e90_76"></div><span  class="e90_77">Home</span>
+    <div class=e90_78><span  class="e90_79">Recommender</span></div><span  class="e90_80">Evaluate Ideas</span><span  class="e90_81">Settings</span><span  class="e90_82">Messages</span>
+    <div class=e90_83><span  class="e90_84">Logo</span></div>
+  </div>
+</div>
+          </body>
+          </html>
