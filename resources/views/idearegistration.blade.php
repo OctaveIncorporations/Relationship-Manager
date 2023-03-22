f19b37d014f444231ecc03f8d135454125608e7a
official-david <adigwu12>
2023-03-22 01:12:00 +0100

    added idea registration page

---
+++ resources/views/idearegistration.blade.php
@@ -0,0 +1,132 @@
+<html lang="en">
+          <head>
+            <meta charset="utf-8">
+          
+            <title>Idea registration</title>
+            <meta name="description" content="Figma htmlGenerator">
+            <meta name="author" content="htmlGenerator">
+            <link href="https://fonts.googleapis.com/css?family=Mulish&display=swap" rel="stylesheet">
+
+            <link rel="stylesheet" href="css/styles.css">
+
+            
+            <style>
+              /*
+                Figma Background for illustrative/preview purposes only.
+                You can remove this style tag with no consequence
+              */
+              body {background: #E5E5E5; }
+            </style>
+          
+          </head>
+          
+          <body>
+            <div class=e91_86>
+  <div class="e91_87"></div><span  class="e91_88">Register Idea</span>
+  <div class=e91_92>
+    <div class="e91_93"></div>
+    <div class=e91_94>
+      <div class="e91_95"></div><span  class="e91_96">Home</span>
+      <div class=e91_97><span  class="e91_98">Recommender</span></div><span  class="e91_99">Evaluate Ideas</span><span  class="e91_100">Settings</span><span  class="e91_101">Messages</span>
+      <div class=e91_102><span  class="e91_103">Logo</span></div>
+    </div>
+  </div>
+<form action="">
+                                <div class="ideareg"><label for="ideaname"><span class="ideareg1">Title</span>
+                                <div >
+                              <input type="text" class="ideareg2" id="ideaname" name="ideaname" placeholder="" required>
+                                    </label>
+
+                                </div>
+                                </div>
+                                <div class="ideareg3"><label for="author"><span class="ideareg4">Author</span>
+                              <div>
+                                <input type="text" class="ideareg5" placeholder="" id="author" name="author" required>
+                                    
+                                    </label>
+
+                                </div>
+                                </div>
+                                <div class="ideareg6"><label for="abstract"><span class="ideareg7">Abstract</span>
+                              <div>
+                                    <textarea class="ideareg8" placeholder="" id="abstract" name="abstract" required></textarea>
+                                    </label>
+                                </div>
+                                </div>
+                                <div class="ideareg9"><label for="publishdate"><span class="ideareg10">Publish Date</span>
+                              <div>
+                                    <input type="date" class="ideareg11" name="publishdate" id="publishdate" required>
+
+                            </div>
+                                    </label>
+                                </div>
+                                
+                                <div class="ideareg12"><label for="expirydate"><span class="ideareg13">Expiry Date</span>
+                              <div>
+                                    <input type="date" class="ideareg14" name="expirydate" id="expirydate" required>
+
+                            </div>
+                                    </label>
+                                </div>
+
+                                <div class="ideareg18"><label for="content"><span class="ideareg19">Content</span>
+<div>
+                                 <textarea class="ideareg20" name="content" id="content" required></textarea>
+                                </div>
+                                   </label>
+                                </div>
+
+                                <div class="ideareg21"><label for="riskrating"><span class="ideareg22">Risk Rating</span>
+                                
+                              <input type="text" class="ideareg23" id="riskrating" name="riskrating" placeholder="" required>
+                                    </label>
+
+                                </div>
+
+                                <div class="ideareg24"><label for="instrument"><span class="ideareg25">Instruments</span>
+                        
+                              <input type="text" class="ideareg26" id="instrument" name="instrument" placeholder="" required>
+                                    </label>
+
+                                </div>
+
+                                <div class="ideareg27"><label for="currency"><span class="ideareg28">Currency</span>
+                        
+                              <input type="text" class="ideareg29" id="currency" name="currency" placeholder="" required>
+                                    </label>
+
+                                </div>
+
+                                <div class="ideareg30"><label for="majorsector"><span class="ideareg31">Major Sector</span>
+                        
+                              <input type="text" class="ideareg32" id="majorsector" name="majorsector" placeholder="" required>
+                                    </label>
+
+                                </div>
+
+                                <div class="ideareg33"><label for="minorsector"><span class="ideareg34">Minor Sector</span>
+                        
+                              <input type="text" class="ideareg35" id="minorsector" name="minorsector" placeholder="" required>
+                                    </label>
+
+                                </div>
+
+                                <div class="ideareg36"><label for="region"><span class="ideareg37">Region</span>
+                        
+                              <input type="text" class="ideareg38" id="region" name="region" placeholder="" required>
+                                    </label>
+
+                                </div>
+
+                                <div class="ideareg39"><label for="country"><span class="ideareg40">Country</span>
+                        
+                              <input type="text" class="ideareg41" id="country" name="country" placeholder="" required>
+                                    </label>
+
+                                </div>
+                            </div>
+<button class="idearegbutton" type= "submit" id="productregister"><span class="idearegbutton1">Register</span></button>
+                            </form>
+ 
+          </body>
+          </html>
